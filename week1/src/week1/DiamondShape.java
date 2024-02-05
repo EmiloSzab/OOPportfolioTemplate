5fab62d0c97d44522e6b5200aa58d36194f08d64
EmiloSzab <szabemil@gmail.com>
2024-02-05 22:04:43 +0000

    week 1 tasks

---
+++ week1/src/week1/DiamondShape.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class DiamondShape {
+	
+	public static void main (String[] args)
+	{
+		System.out.println("   *\n  ***\n *****\n*******\n *****\n  ***\n   *");
+	}
+	
+
+}
---
+++ week1/src/week1/PersonalDetails.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class PersonalDetails {
+	
+	public static void main (String [] args)
+	{
+		System.out.println ("Personal details:\nName: Mike\nSurname: Wahousekey");
+	
+	}
+
+}
